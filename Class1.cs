375232a63544debb596941884ae97c49f487f621
tssuru <115495515+tssuru@users.noreply.github.com>
2023-09-27 18:27:14 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace HW_2._2_oop
+{
+    abstract class Worker
+    {
+
+        public string _name;
+        public string _position;
+        public string _workDay;
+
+
+        public void Call()
+        {
+
+        }
+
+        public void WriteCode()
+        {
+
+        }
+
+        public void Relax()
+        {
+
+        }
+        public abstract void FillWorkDay();
+    }
+
+    class Developer : Worker
+    {
+        public Developer(string name, string position, string day)
+        {
+            _position = position;
+            _name = name;
+            _workDay= day;
+        }
+
+        public override void FillWorkDay()
+        {
+            WriteCode();
+            Call();
+            Relax();
+            WriteCode();
+        }
+    }
+
+    class Manager : Worker
+    {
+        private Random _random;
+
+        public Manager(string name, string position, string day)
+        {
+            _position = position;
+            _name = name;
+            _workDay = day;
+        }
+    
+        public override void FillWorkDay()
+        {
+            for (int i = 0; i < _random.NextInt64(10) + 1; i++)
+            {
+                Call();
+            }
+
+            Relax();
+
+            for (int i = 0; i < _random.NextInt64(5) + 1; i++)
+            {
+                Call();
+            }
+        }
+
+    }
+
+    class Team
+    {
+        private string _teamName;
+        private List<Worker> _workers = new List<Worker>();
+        public Team(string teamName)
+        {
+            _teamName = teamName;
+        }
+
+        public void AddWorker(Worker worker)
+        {
+            _workers.Add(worker);
+        }
+
+        public void WatchInf()
+        {
+            Console.WriteLine(_teamName);
+            foreach (Worker worker in _workers)
+            {
+                Console.WriteLine(worker._name);
+            }
+        }
+
+        public void GetDetailInf()
+        {
+            Console.WriteLine(_teamName);
+            foreach (Worker worker in _workers)
+            {
+                Console.WriteLine($"<{worker._name}> - <{worker._position}> - <{worker._workDay}>");                
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using HW_2._2_oop;
+
+
+Console.WriteLine("Enter Team Name:");
+string teamName = Console.ReadLine();
+Team team = new Team(teamName);
+
+while (true)
+{
+    Console.WriteLine("Choose option: (1) Add Devops, (2) Add Manager, (3) Show team, (4) Show information about members in team, (5) Exit");
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine("Enter developer`s name, position, workday:");
+            string devName = Console.ReadLine();
+            string devPos = Console.ReadLine();
+            string devWD = Console.ReadLine();
+            Developer dev = new Developer(devName, devPos, devWD);
+            team.AddWorker(dev);
+            break;
+
+        case "2":
+            Console.WriteLine("Enter meneger`s name, position, workday:");
+            string mgrName = Console.ReadLine();
+            string mgrPos = Console.ReadLine();
+            string mgrDW = Console.ReadLine();
+            Manager mgr = new Manager(mgrName, mgrPos, mgrDW);
+            team.AddWorker(mgr);
+            break;
+
+        case "3":
+            team.WatchInf();
+            break;
+
+        case "4":
+            team.GetDetailInf();
+            break;
+
+        case "5":
+            return;
+
+        default:
+            Console.WriteLine("Uncorrect answer, try again.");
+            break;
+    }
+}
+
